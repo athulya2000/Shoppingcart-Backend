fdb9a4504b8f2cc38394563316de449087b60927
athulya2000 <athulyav00@gmail.com>
2022-12-07 20:09:00 +0530

    search

--- src/main/java/com/example/shoppingcartapp_backend/dao/ShoppingcartDao.java
+++ src/main/java/com/example/shoppingcartapp_backend/dao/ShoppingcartDao.java
@@ -1,7 +1,13 @@
 package com.example.shoppingcartapp_backend.dao;
 
 import com.example.shoppingcartapp_backend.model.Shoppingcart;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ShoppingcartDao extends CrudRepository<Shoppingcart,Integer> {
+    @Query(value="SELECT `id`, `category`, `description`, `image`, `name`, `price` FROM `products` WHERE `name`=:name",nativeQuery = true)
+    List<Shoppingcart> SearchProduct(@Param("name") String name);
 }
